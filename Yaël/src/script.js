3302bb0f03aeb8d266a6dfd7dd69e3743a5cdab2
Yaël Pierre <yael.pierre@epitech.eu>
2026-03-04 15:23:02 +0100

    [Features] GSAP and starting to think
    Added gsap

--- Yaël/src/script.js
+++ Yaël/src/script.js
@@ -1,11 +1,6 @@
 import * as THREE from 'three'
 
-const AppleCount = 0;
-
-document.addEventListener('DOMContentLoaded', () => {
-    generateApples(AppleCount);
-    AppleCount = Math.floor(Math.random() * 5) + 5;
-});
+let leftPart = 300
 
 // Canvas
 const canvas = document.getElementById('right_content_bg_canva')
@@ -15,16 +10,14 @@ const scene = new THREE.Scene()
 
 // Tailles
 const sizes = {
-    width: window.innerWidth - 300, // Largeur - left_profile (300px)
+    width: window.innerWidth - leftPart,
     height: window.innerHeight
 }
 
-// Caméra (statique pour le momment)
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
 
-// Renderer avec fond transparent
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     alpha: true,
@@ -33,9 +26,8 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Handle le ressize
 window.addEventListener('resize', () => {
-    sizes.width = window.innerWidth - 300
+    sizes.width = window.innerWidth - leftPart
     sizes.height = window.innerHeight
     
     camera.aspect = sizes.width / sizes.height
@@ -45,16 +37,8 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-function generateApples(count) {
-    // je vais générer mes pommes ici
-}
-
-// Animation loop
 const animate = () => {
     requestAnimationFrame(animate)
-    
-    // Anims ici
-    
     renderer.render(scene, camera)
 }
 
